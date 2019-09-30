95a8270d0944a1effe4f8115c0b001699d6dd346
Kari <42503401+KJLarson@users.noreply.github.com>
2019-09-30 12:34:05 -0500

    Add cloneNode() to create titles for damage relation types
    Loop through damageRelations and use cloneNode() to create h3 titles and ULs for each damage relation.

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ fetch(fireURL)
     const halfDamageTo = data.damage_relations.half_damage_to;
     const noDamageFrom = data.damage_relations.no_damage_from;
     const noDamageTo = data.damage_relations.no_damage_to;
+    let h3 = createNode("h3");
+    let ul = createNode("ul");
+    let li = createNode("li");
+    let cloneH3;
+    let cloneUL;
+    let cloneLI;
 
     const damageRelationsArr = [
       doubleDamageFrom,
@@ -47,6 +53,39 @@ fetch(fireURL)
     //   append(doubleDamageFromList, relatedType);
     // }
     // ---------------------------------------
+
+    // Loop through all damage relations for Fire type
+    // for (let i = 0; i < damageRelations.length; i++) {
+    //   let damageRelationTitle = createNode("h3");
+    //   damageRelationTitle.textContent = damageRelations[i];
+    //   append(app, damageRelationTitle);
+    // }
+
+    for (var key in damageRelations) {
+      if (damageRelations.hasOwnProperty(key)) {
+        // console.log(damageRelations[key]);
+        let damageTypes = damageRelations[key];
+
+        // Create title for each damage relation
+        console.log(key);
+        cloneH3 = h3.cloneNode();
+        cloneH3.textContent = key;
+        append(app, cloneH3);
+
+        // Create list for each damage relation
+        console.log(damageTypes);
+        cloneUL = ul.cloneNode();
+        cloneUL.className = "key";
+        append(app, cloneUL);
+
+        // Create list item for each type in damage relation
+        cloneLI = li.cloneNode();
+
+        // if (damageTypes.hasOwnProperty(key)) {
+        //   // console.log(damageTypes[key]);
+        // }
+      }
+    }
   })
   .catch(error => console.log(error));
 
